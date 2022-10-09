9922ef1f976953894e47b3dde23ba37d03c40b78
floroz <tortora87@gmail.com>
2022-10-09 10:06:17 +0200

    add glob

--- index.mjs
+++ index.mjs
@@ -7,20 +7,21 @@ import { fileURLToPath } from "url";
 const root = path.dirname(fileURLToPath(import.meta.url));
 
 const hasteMapOptions = {
-  extensions: ["js"],
-  maxWorkers: cpus().length,
-  name: "best-test-framework",
-  platforms: [],
-  rootDir: root,
-  roots: [root],
+  extensions: ["js"], // Tells jest-haste-map to only crawl .js files.
+  maxWorkers: cpus().length, // Parallelizes across all available CPUs.
+  name: "best-test-framework", // Used for caching.
+  platforms: [], // This is only used for React Native, leave empty.
+  rootDir: root, // The project root.
+  roots: [root], // Can be used to only search a subset of files within `rootDir`.
 };
 // Need to use `.default` as of Jest 27.
 const hasteMap = new JestHasteMap.default(hasteMapOptions);
 // This line is only necessary in `jest-haste-map` version 28 or later.
 await hasteMap.setupCachePath(hasteMapOptions);
-
+// Build and return an in-memory HasteFS ("Haste File System") instance.
 const { hasteFS } = await hasteMap.build();
-const testFiles = hasteFS.getAllFiles();
+
+const testFiles = hasteFS.matchFilesWithGlob(["**/*.test.js"]);
 
 console.log(testFiles);
 // ['/path/to/tests/01.test.js', '/path/to/tests/02.test.js', …]
